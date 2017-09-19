398eab531b16a17dbed64baac8cf1b642156d9b4
Dana Wortman <danawortman@gmail.com>
2017-09-18 22:30:24 -0600

    Added main character and ground plane to Main scene.  Created Main scene.  Added material to character and ground.

---
+++ Tutorial/Assets/CapsuleCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleCharacter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            gameObject.GetComponent<Rigidbody>().transform.Translate(0, 0, 0.25f);
+        }
+	}
+}
