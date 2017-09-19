380d24eac5adb8a082079c0678e0c6d2d4190fd3
Dana Wortman <danawortman@gmail.com>
2017-09-19 01:39:14 -0600

    Added alternative keys (enter, keypadenter, and N) for character jump.

--- Tutorial/Assets/CapsuleCharacter.cs
+++ Tutorial/Assets/CapsuleCharacter.cs
@@ -27,7 +27,7 @@ public class CapsuleCharacter : MonoBehaviour {
         {
             gameObject.GetComponent<Rigidbody>().transform.Translate(-0.25f, 0, 0);
         }
-        if (Input.GetKey(KeyCode.Return))
+        if (Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.KeypadEnter) || Input.GetKey(KeyCode.N))
         {
             gameObject.GetComponent<Rigidbody>().transform.Translate(0, 0.25f, 0);
         }
