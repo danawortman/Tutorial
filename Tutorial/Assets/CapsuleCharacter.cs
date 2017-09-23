5ba0cbeb308a45d3f6a45f4037970e9dfc1361c0
Dana Wortman <danawortman@gmail.com>
2017-09-23 01:46:55 -0600

    Removed left and right movement of capsule, added "run" for capsule with shift.

--- Tutorial/Assets/CapsuleCharacter.cs
+++ Tutorial/Assets/CapsuleCharacter.cs
@@ -13,19 +13,20 @@ public class CapsuleCharacter : MonoBehaviour {
 	void Update () {
         if (Input.GetKey(KeyCode.UpArrow))
         {
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                gameObject.GetComponent<Rigidbody>().transform.Translate(0, 0, 0.25f);
+            }
             gameObject.GetComponent<Rigidbody>().transform.Translate(0, 0, 0.25f);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                gameObject.GetComponent<Rigidbody>().transform.Translate(0, 0, -0.25f);
+            }
             gameObject.GetComponent<Rigidbody>().transform.Translate(0, 0, -0.25f);
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            gameObject.GetComponent<Rigidbody>().transform.Translate(-0.25f, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            gameObject.GetComponent<Rigidbody>().transform.Translate(0.25f, 0, 0);
-        }
+
     }
 }
